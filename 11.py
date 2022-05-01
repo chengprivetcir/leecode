f3c926a42f6441a7a7f41b56fd558289570ff5f9
Cheng Wu <cwu@tmhcc.com>
2022-04-30 22:54:17 -0400

    11

---
+++ 11.py
@@ -0,0 +1,13 @@
+height = [1,8,6,2,5,4,8,3,7]
+
+res = 0
+
+l,r = 0,len(height)-1
+while l < r:
+    area = (r-l)*min(height[l],height[r])
+    res = max(res, area)
+    if height[l]<height[r]:
+        l+=1
+    else:
+        r-=1
+print(res)
