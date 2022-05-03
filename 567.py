37872688802f31b30bb4c6fb010f49fee95fbbcc
Cheng Wu <cwu@tmhcc.com>
2022-05-03 15:09:35 -0400

    567

---
+++ 567.py
@@ -0,0 +1,39 @@
+s1 = "ab"
+s2 = "eidbaooo"
+
+
+# dic1={}
+#
+# for i in range(len(s1)):
+#     dic1[s1[i]] = 1+dic1.get(s1[i],0)
+#
+# l=0
+# for r in range(len(s1)-1,len(s2)):
+#     dic = {}
+#
+#     for i in range(l,r+1):
+#         dic[s2[i]] = 1+dic.get(s2[i],0)
+#         print(dic)
+#
+#     if dic == dic1:
+#         print(True)
+#     else:
+#
+#         l+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
